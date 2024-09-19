ed32e3b59bcc500e0258420d81c6838cca0f5d25
Jose Pedro Miranda Goncalves <jomirand@c2r5s11.42porto.com>
2024-09-19 17:39:27 +0100

    added re_path to urls.py

--- transcendence_docker/django/project_files/firstApp/urls.py
+++ transcendence_docker/django/project_files/firstApp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
+    re_path(r'^.*$', views.homepage),
 ]
