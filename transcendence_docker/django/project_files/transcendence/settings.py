ef989142d9c9bf08b0a108563a7f07c586ad1244
macastan <mluizacmarques@gmail.com>
2024-10-07 11:58:22 +0100

    settings 0.0.0.0

--- transcendence_docker/django/project_files/transcendence/settings.py
+++ transcendence_docker/django/project_files/transcendence/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oo@oma8qn$u6t@ws^%i576%p(7)@qj!ql0psqhq8fyqsta&(!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '0.0.0.0', ]
 
 
 # Application definition
