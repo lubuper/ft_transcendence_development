1ad772876fff72920f904d3d67f7debaaeefdc06
Jose Pedro Miranda Goncalves <jomirand@c1r7s5.42porto.com>
2024-10-08 19:08:41 +0100

    slight updates

--- transcendence_docker/django/project_files/templates/static/js/components/pages/Dashboard.js
+++ transcendence_docker/django/project_files/templates/static/js/components/pages/Dashboard.js
@@ -71,6 +71,7 @@ export default function DashBoard() {
 					<div class="col-md-3">
 						<div class="card bg-dark text-white mb-3">
 							<div class="card-header">Friends</div>
+							<input type="text" class="form-control mb-3" id="searchInput" placeholder="Search friends to add...">
 							<div class="card-body">
 								<p>Friend 1</p>
 								<p>Friend 2</p>
