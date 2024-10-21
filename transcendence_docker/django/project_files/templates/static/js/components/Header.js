8456399a8c28449d9836c41e6b85fd6a982a7000
Luis Pereira <lde-sous@student.42porto.com>
2024-10-21 19:04:38 +0100

    music player working but not auto. Local play layout changed

--- transcendence_docker/django/project_files/templates/static/js/components/Header.js
+++ transcendence_docker/django/project_files/templates/static/js/components/Header.js
@@ -17,7 +17,7 @@ export default function Header() {
 			style="pointer-events: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1;">
 			<source src="/static/media/assets/1.mp4" type="video/mp4">
 		</video>
-	`
+	`;
 
 	$header.innerHTML = base;
 
@@ -50,25 +50,34 @@ export default function Header() {
 		}, 100);
 	});
 
+	// Handle the ended event to loop the track or play the next one
+	audioPlayer.addEventListener('ended', function() {
+		const selectedIndex = musicSelection.selectedIndex;
+		const nextIndex = (selectedIndex + 1) % musicSelection.options.length;
+		musicSelection.selectedIndex = nextIndex;
+		audioPlayer.src = musicSelection.value;
+		audioPlayer.play();
+	});
+
 	fetch('/current-user/')
 		.then(async (response) => {
 			const user = await response.json();
 
-			console.log( 'user', user)
+			console.log('user', user);
 
 			$header.innerHTML = `
 			<nav class="navbar navbar-expand-lg d-flex justify-content-between" style="background: transparent;">
-            <h1 class="fw-bold text-center text-white display-6 ms-3" data-path="/">ft_transcendence</h1>
-            <div class="d-flex align-items-center">
-                <button class="btn btn-purple btn-custom mx-1 nav-link text-white shadow-lg d-flex align-items-center" data-path="/profile">
-                    <img src="/static/${user.profile_picture}" alt="${user.username}'s profile picture" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 8px;">
-                    ${user.username}
-                </button>
-                <button class="btn btn-danger btn-custom mx-1 nav-link text-white shadow-lg" id="logout-btn">Logout</button>
-                <button class="btn btn-purple btn-custom mx-1 nav-link text-white shadow-lg" data-path="/dashboard">Dashboard</button>
-            </div>
-        </nav>
-        	${base}
+			<h1 class="fw-bold text-center text-white display-6 ms-3" data-path="/">ft_transcendence</h1>
+			<div class="d-flex align-items-center">
+				<button class="btn btn-purple btn-custom mx-1 nav-link text-white shadow-lg d-flex align-items-center" data-path="/profile">
+					<img src="/static/${user.profile_picture}" alt="${user.username}'s profile picture" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 8px;">
+					${user.username}
+				</button>
+				<button class="btn btn-danger btn-custom mx-1 nav-link text-white shadow-lg" id="logout-btn">Logout</button>
+				<button class="btn btn-purple btn-custom mx-1 nav-link text-white shadow-lg" data-path="/dashboard">Dashboard</button>
+			</div>
+		</nav>
+			${base}
 	`;
 
 			if (user.username) {
@@ -76,14 +85,52 @@ export default function Header() {
 				logoutButton.addEventListener('click', async () => {
 					await fetch('/logout/', { method: 'POST', credentials: 'same-origin' });
 					setTimeout(() => {
-						window.location.href = '/';  // Redirect to home page after 1 seconds
+						window.location.href = '/';  // Redirect to home page after 1 second
 					}, 1000);
 				});
 			}
 
+			// Reattach event listeners after updating the innerHTML
+			const audioPlayer = $header.querySelector('#audioplayer');
+			const musicSelection = $header.querySelector('#musicSelection');
+
+			audioPlayer.volume = 0.2;
+
+			musicSelection.addEventListener('change', function() {
+				// Fade out the current track
+				let currentVolume = audioPlayer.volume;
+				const fadeOutInterval = setInterval(() => {
+					if (currentVolume > 0.05) {
+						currentVolume -= 0.05;
+						audioPlayer.volume = currentVolume;
+					} else {
+						clearInterval(fadeOutInterval);
+						audioPlayer.src = this.value;
+						audioPlayer.volume = 0; // Start with volume at 0
+						audioPlayer.play();
+
+						const fadeInInterval = setInterval(() => {
+							if (audioPlayer.volume < 0.2) {
+								audioPlayer.volume += 0.05;
+							} else {
+								clearInterval(fadeInInterval);
+							}
+						}, 100);
+					}
+				}, 100);
+			});
+
+			audioPlayer.addEventListener('ended', function() {
+				const selectedIndex = musicSelection.selectedIndex;
+				const nextIndex = (selectedIndex + 1) % musicSelection.options.length;
+				musicSelection.selectedIndex = nextIndex;
+				audioPlayer.src = musicSelection.value;
+				audioPlayer.play();
+			});
+
 		})
 		.catch((error) => {
-			console.log("error", error)
+			console.log("error", error);
 			$header.innerHTML = `
 			<nav class="navbar navbar-expand-lg d-flex justify-content-between" style="background: transparent;">
 				<h1 class="fw-bold text-center text-white display-6 ms-3" data-path="/">ft_transcendence</h1>
@@ -93,7 +140,46 @@ export default function Header() {
 				</div>
 			</nav>
 			${base}
-			`
-		})
+			`;
+
+			// Reattach event listeners after updating the innerHTML
+			const audioPlayer = $header.querySelector('#audioplayer');
+			const musicSelection = $header.querySelector('#musicSelection');
+
+			audioPlayer.volume = 0.2;
+
+			musicSelection.addEventListener('change', function() {
+				// Fade out the current track
+				let currentVolume = audioPlayer.volume;
+				const fadeOutInterval = setInterval(() => {
+					if (currentVolume > 0.05) {
+						currentVolume -= 0.05;
+						audioPlayer.volume = currentVolume;
+					} else {
+						clearInterval(fadeOutInterval);
+						audioPlayer.src = this.value;
+						audioPlayer.volume = 0; // Start with volume at 0
+						audioPlayer.play();
+
+						const fadeInInterval = setInterval(() => {
+							if (audioPlayer.volume < 0.2) {
+								audioPlayer.volume += 0.05;
+							} else {
+								clearInterval(fadeInInterval);
+							}
+						}, 100);
+					}
+				}, 100);
+			});
+
+			audioPlayer.addEventListener('ended', function() {
+				const selectedIndex = musicSelection.selectedIndex;
+				const nextIndex = (selectedIndex + 1) % musicSelection.options.length;
+				musicSelection.selectedIndex = nextIndex;
+				audioPlayer.src = musicSelection.value;
+				audioPlayer.play();
+			});
+		});
+
 	return $header;
 }
--- transcendence_docker/django/project_files/templates/static/js/components/pages/LocalPlay.js
+++ transcendence_docker/django/project_files/templates/static/js/components/pages/LocalPlay.js
@@ -2,117 +2,125 @@ let selectedGameMode = null;
 let selectedGameType = null;
 
 export default function LocalPlay(navigate) {
-    const $games = document.createElement('div');
-    $games.innerHTML = `
-        <div class="container vh-100 d-flex align-items-center justify-content-center">
-            <div class="row w-100">
-                <div class="col-md-4 text-center d-flex flex-column align-items-center">
-                    <img src="/static/media/assets/pongsplash.png" alt="Pong Game" class="img-fluid mt-2" style="width: 600px; height: auto;">
-                    <div class="card bg-dark text-white mt-3" style="width: 18rem; border: 1px solid #343a40; opacity: 0.8;">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">How To Play</h5>
-                            <p class="card-text">
-                                Player 1: A and D<br>
-                                Player 2: J and L
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 d-flex flex-column align-items-center">
-                    <div class="card mb-3 bg-dark text-white w-100" style="border: 1px solid #343a40; opacity: 0.8;">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Select Game Mode</h5>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gameMode" id="pvp" value="1" checked>
-                                <label class="form-check-label" for="pvp">Player vs Player</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gameMode" id="ai" value="2">
-                                <label class="form-check-label" for="ai">Play vs AI</label>
-                            </div>
-                            <div id="difficultyOptions" class="mt-3" style="display: none;">
-                                <h6 class="text-center">Select Difficulty</h6>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gameMode" id="ai-easy" value="2">
-                                    <label class="form-check-label" for="ai-easy">Easy</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gameMode" id="ai-medium" value="3">
-                                    <label class="form-check-label" for="ai-medium">Medium</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gameMode" id="ai-hard" value="4">
-                                    <label class="form-check-label" for="ai-hard">Hard</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="gameMode" id="ai-impossible" value="5">
-                                    <label class="form-check-label" for="ai-impossible">Impossible</label>
-                                </div>
-                            </div>
-                            <h5 class="card-title text-center mt-3">Select Game Type</h5>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gameType" id="default" value="default" checked>
-                                <label class="form-check-label" for="default">Default</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gameType" id="powered-up" value="powered">
-                                <label class="form-check-label" for="powered-up">Powered Up</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center mt-4">
-                        <button class="btn btn-purple btn-lg mx-5 text-white shadow-lg" data-path="/pong">Pong</button>
-                        <button class="btn btn-purple btn-lg mx-5 text-white shadow-lg" data-path="/asteroids">Asteroids</button>
-                    </div>
-                </div>
-                <div class="col-md-4 text-center d-flex flex-column align-items-center">
-                    <img src="/static/media/assets/asteroidssplash.png" alt="Asteroids Game" class="img-fluid mt-2" style="width: 600px; height: auto;">
-                    <div class="card bg-dark text-white mt-3" style="width: 18rem; border: 1px solid #343a40; opacity: 0.8;">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">How To Play</h5>
-                            <p class="card-text">
-                                Rotate left: A<br>
-                                Rotate right: D<br>
-                                Fire: Space<br>
-                                Shields: E
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
+	const $games = document.createElement('div');
+	$games.innerHTML = `
+		<div class="container vh-100 d-flex align-items-center justify-content-center">
+			<div class="row w-100">
+				<!-- Pong Section -->
+				<div class="col-md-6 text-center d-flex flex-column align-items-center">
+					<img src="/static/media/assets/pongsplash.png" alt="Pong Game" class="img-fluid mt-2" style="width: 100%; height: auto;">
+					<div class="row w-100 mt-3">
+						<div class="col-md-6">
+							<div class="card bg-dark text-white w-100" style="border: 1px solid #343a40; opacity: 0.8;">
+								<div class="card-body">
+									<h5 class="card-title text-center">How To Play</h5>
+									<p class="card-text">
+										Player 1: A and D<br>
+										Player 2: J and L
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="card bg-dark text-white w-100" style="border: 1px solid #343a40; opacity: 0.8;">
+								<div class="card-body">
+									<h5 class="card-title text-center">Select Game Mode</h5>
+									<div class="form-check">
+										<input class="form-check-input" type="radio" name="gameMode" id="pvp" value="1" checked>
+										<label class="form-check-label" for="pvp">Player vs Player</label>
+									</div>
+									<div class="form-check">
+										<input class="form-check-input" type="radio" name="gameMode" id="ai" value="2">
+										<label class="form-check-label" for="ai">Play vs AI</label>
+									</div>
+									<div id="difficultyOptions" class="mt-3" style="display: none;">
+										<h6 class="text-center">Select Difficulty</h6>
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="gameMode" id="ai-easy" value="2">
+											<label class="form-check-label" for="ai-easy">Easy</label>
+										</div>
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="gameMode" id="ai-medium" value="3">
+											<label class="form-check-label" for="ai-medium">Medium</label>
+										</div>
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="gameMode" id="ai-hard" value="4">
+											<label class="form-check-label" for="ai-hard">Hard</label>
+										</div>
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="gameMode" id="ai-impossible" value="5">
+											<label class="form-check-label" for="ai-impossible">Impossible</label>
+										</div>
+									</div>
+									<h5 class="card-title text-center mt-3">Select Game Type</h5>
+									<div class="form-check">
+										<input class="form-check-input" type="radio" name="gameType" id="default" value="default" checked>
+										<label class="form-check-label" for="default">Default</label>
+									</div>
+									<div class="form-check">
+										<input class="form-check-input" type="radio" name="gameType" id="powered-up" value="powered">
+										<label class="form-check-label" for="powered-up">Powered Up</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="d-flex justify-content-center mt-4">
+						<button class="btn btn-purple btn-lg mx-5 text-white shadow-lg" data-path="/pong">Pong</button>
+					</div>
+				</div>
+				<!-- Asteroids Section -->
+				<div class="col-md-6 text-center d-flex flex-column align-items-center">
+					<img src="/static/media/assets/asteroidssplash.png" alt="Asteroids Game" class="img-fluid mt-2" style="width: 100%; height: auto;">
+					<div class="card bg-dark text-white mt-3 w-100" style="border: 1px solid #343a40; opacity: 0.8;">
+						<div class="card-body">
+							<h5 class="card-title text-center">How To Play</h5>
+							<p class="card-text">
+								Rotate left: A<br>
+								Rotate right: D<br>
+								Fire: Space<br>
+								Shields: E
+							</p>
+						</div>
+					</div>
+					<div class="d-flex justify-content-center mt-4">
+						<button class="btn btn-purple btn-lg mx-5 text-white shadow-lg" data-path="/asteroids">Asteroids</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	`;
 
-    // Event listener to show/hide difficulty options
-    $games.addEventListener('change', (event) => {
-        if (event.target.name === 'gameMode') {
-            const difficultyOptions = document.getElementById('difficultyOptions');
-            const selectedValue = document.querySelector('input[name="gameMode"]:checked').value;
-            if (selectedValue === '2') {
-                difficultyOptions.style.display = 'block';
-            } else if (selectedValue === '1') {
-                difficultyOptions.style.display = 'none';
-            }
-        }
-    });
+	// Event listener to show/hide difficulty options
+	$games.addEventListener('change', (event) => {
+		if (event.target.name === 'gameMode') {
+			const difficultyOptions = document.getElementById('difficultyOptions');
+			const selectedValue = document.querySelector('input[name="gameMode"]:checked').value;
+			if (selectedValue === '2') {
+				difficultyOptions.style.display = 'block';
+			} else if (selectedValue === '1') {
+				difficultyOptions.style.display = 'none';
+			}
+		}
+	});
 
-    $games.addEventListener('click', (event) => {
-        if (event.target.matches('[data-path]')) {
-            event.preventDefault();
-            const path = event.target.getAttribute('data-path');
-            selectedGameMode = document.querySelector('input[name="gameMode"]:checked').value;
-            selectedGameType = document.querySelector('input[name="gameType"]:checked').value;
-            navigate(path);
-        }
-    });
+	$games.addEventListener('click', (event) => {
+		if (event.target.matches('[data-path]')) {
+			event.preventDefault();
+			const path = event.target.getAttribute('data-path');
+			selectedGameMode = document.querySelector('input[name="gameMode"]:checked').value;
+			selectedGameType = document.querySelector('input[name="gameType"]:checked').value;
+			navigate(path);
+		}
+	});
 
-    return $games;
+	return $games;
 }
 
 export function getSelectedGameMode() {
-    return selectedGameMode;
+	return selectedGameMode;
 }
 
 export function getSelectedGameType() {
-    return selectedGameType;
+	return selectedGameType;
 }
