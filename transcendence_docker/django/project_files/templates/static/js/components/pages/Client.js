f0e196ad864b20801567daa0235a7babae6cff4c
zpedro99 <zpedro99@ZPedroVM.42.porto>
2024-11-08 18:48:50 +0000

    Friend Chat working!

--- transcendence_docker/django/project_files/templates/static/js/components/pages/Client.js
+++ transcendence_docker/django/project_files/templates/static/js/components/pages/Client.js
@@ -7,6 +7,11 @@ export function setupChat(friendName, userName) {
         const data = JSON.parse(e.data);
         const newMessage = document.createElement('p');
         newMessage.textContent = `${data.sender} : ${data.message}`;
+        if (data.sender === userName) {
+            newMessage.classList.add('my-message');
+        } else if (data.sender === friendName) {
+            newMessage.classList.add('friend-message');
+        }
         messageContainer.appendChild(newMessage);
         messageContainer.scrollTop = messageContainer.scrollHeight;
     };
@@ -19,24 +24,22 @@ export function setupChat(friendName, userName) {
         console.error('Chat socket error', e);
     };
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const senderButton = document.querySelector(`#chat-box-${friendName} .sender`);
-        const inputField = document.querySelector(`#chat-box-${friendName} .message-input`);
+    const senderButton = document.querySelector(`.sender`);
+    const inputField = document.querySelector(`.message-input`);
 
-        if (senderButton && inputField) {
-            senderButton.addEventListener('click', () => {
-                if (inputField.value.trim() !== '') {
-                    chatSocket.send(JSON.stringify({ 'message': inputField.value }));
-                    inputField.value = ''; // Clear the input field after sending
-                }
-            });
+    if (senderButton && inputField) {
+        senderButton.addEventListener('click', () => {
+            if (inputField.value.trim() !== '') {
+                chatSocket.send(JSON.stringify({ 'message': inputField.value }));
+                inputField.value = ''; // Clear the input field after sending
+            }
+        });
 
-            inputField.addEventListener('keyup', function(e) {
-                if (e.key === 'Enter') {
-                    senderButton.click();
-                    console.log('Message sent');
-                }
-            });
-        }
-    });
+        inputField.addEventListener('keyup', function(e) {
+            if (e.key === 'Enter') {
+                senderButton.click();
+                console.log('Message sent');
+            }
+        });
+    }
 }
